688bd6fba2eac9b19832d5f5b183c9d44ad64eba
brian3t <ngxtri@gmail.com>
2019-04-13 14:26:59 -0700

    Add missing tinymce

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -124,11 +124,11 @@ AppAsset::register($this);
                 width: 600,
                 height: 300,
                 plugins: [
-                    'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+                    'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker code',
                     'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
                     'save table contextmenu directionality emoticons template paste textcolor'
                 ],
-                toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+                toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons code'
             });
 
 
